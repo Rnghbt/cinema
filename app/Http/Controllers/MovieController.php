ca84c57997afbfa84e81bce3b1e7ef5a85c83807
rnghbt <rnghbt23@gmail.com>
2024-04-24 02:00:40 +0700

    first commit

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $search = $request->search;
+        $movies = Movie::latest()
+            ->where('title', 'like', '%' . $search . '%')
+            ->get();
+        return view(
+            'movies.index',
+            ['movies' => $movies]
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('movies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Movie;
+
+class MovieFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Movie::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(4),
+            'year' => $this->faker->word(),
+            'actors' => $this->faker->word(),
+            'sinopsis' => $this->faker->text(),
+            'description' => $this->faker->text(),
+            'durasi' => $this->faker->text(),
+            'poster' => $this->faker->word(),
+            'trailer' => $this->faker->word(),
+        ];
+    }
+}
