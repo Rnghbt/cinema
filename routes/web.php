a95abda712e84535c983ff6784b93472306e038c
rnghbt <rnghbt23@gmail.com>
2024-04-24 02:06:22 +0700

    njn

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('/admin/cinematics', [MovieController::class]);
+    // Route::resource('/admin/cinematics', [MovieController::class]);
     // Route::resource('/admin/genres' [GenreController::class]);
     // Route::resource('/admin/ratings' [RatingController::class]);
 });
